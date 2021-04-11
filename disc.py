6268c83735605909ee619206e7ea4774c092ef88
Miguel Sheker <migs0223@gmail.com>
2021-04-11 04:00:24 -0700

    \\0x version working, >0x failed

--- disc.py
+++ disc.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 from keys import DISCORD_TOKEN
 
 client = discord.Client()
@@ -12,8 +13,39 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('\0x '):
-        num = message.content.split(" ")[1]
-        await message.channel.send(num)
+    if message.content.startswith('\\0x '):
+        num = message.content.split(" ")[1].lower()
+        res = 0
+        abc = "0123456789abcdef"
+        for x in num:
+            res = res * 16
+            if x not in abc:
+                await messsage.channel.send("ERROR: Expected hex, got: " + num)
+                return
+            res = res + abc.index(x)
+        print(num,"->",res)
+        await message.channel.send(str(res))
+
+bot = commands.Bot(command_prefix=">")
+
+@bot.command(name='0x', help="Hex to decimal converter")
+async def _0x(ctx, num):
+    num = num.lower()
+    res = 0
+    abc = "0123456789abcdef"
+    for x in num:
+        res = res * 10
+        if x not in abc:
+            await ctx.send("ERROR: Expected hex, got: " + num)
+            return
+        res = res + abc.index(x)
+        print(num,"->",res)
+    await ctx.send(str(res))
+
+@bot.command(name="resume", help="Provides resume in ppdf format")
+async def _resume(ctx):
+
+    await ctx.send()
+
 
 client.run(DISCORD_TOKEN)
