f8cddf9d1eecc59d97185df95e4f8bd58e08e4c0
Miguel Sheker <migs0223@gmail.com>
2025-06-20 03:16:34 -0700

    added background task to check for online

--- disc.py
+++ disc.py
@@ -1,14 +1,24 @@
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 from keys import DISCORD_TOKEN
 import requests
 from bs4 import BeautifulSoup as bs
 
-bot = commands.Bot(command_prefix="\\")
+intents = discord.Intents.default()
+intents.message_content = True  # Needed if your bot reads message text
+intents.members = True  # Optional: needed for member-related events or attributes
+intents.presences = True
+
+bot = commands.Bot(command_prefix="\\", intents=intents)
+
+last = False
 
 @bot.event
 async def on_ready():
     print("Connected")
+    last = False
+    check.start()
     return
 
 @bot.command(name='0x', help="Hex to decimal converter")
@@ -41,5 +51,17 @@ async def _ow(ctx):
     [tnk,dps,sup] = [tnk.text,dps.text,sup.text]
     print(f"Tank: {tnk}; DPS: {dps}; Support: {sup}")
     await ctx.send(f"Tank: {tnk}\n DPS: {dps}\n Support: {sup}")
+
+@tasks.loop(seconds=300)
+async def check():
+    g = bot.get_guild(470050245997363220)
+    mad = g.get_member(993412113517248603)
+    mig = g.get_member(277291651486187520)
+
+    if mad.client_status.desktop == 'online' and not last:
+        await ctx.send(f'{mig.mention} :eyes:')
+
+    last = mad.client_status.desktop == 'online'
+    print(last)
 
 bot.run(DISCORD_TOKEN)
